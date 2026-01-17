f2e2c4b6aceea5202d4c2bc8b90b1a27d4b1faf4
Ryan Achdiadsyah <achdiadsyah@gmail.com>
2026-01-17 22:27:47 +0700

    chore(auth): add basic auth view routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('auth.forgot-password');
-});
+Route::view('/login', 'auth.login')->name('login');
+Route::view('/register', 'auth.register')->name('register');
+Route::view('/forgot-password', 'auth.forgot-password')->name('password.request');
