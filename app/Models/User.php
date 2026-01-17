224a0439b1ce7fb9163bf5ccb2f366b26021fb50
Ryan Achdiadsyah <achdiadsyah@gmail.com>
2026-01-17 22:51:16 +0700

    feat(otp): create otp_verifications table

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,6 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
         'phone_number',
         'password',
     ];
@@ -42,7 +41,7 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
+            'phone_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
