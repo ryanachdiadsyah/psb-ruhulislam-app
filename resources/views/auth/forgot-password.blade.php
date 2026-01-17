f9519260cb4e3659944c827a9001ea9b6393a802
Ryan Achdiadsyah <achdiadsyah@gmail.com>
2026-01-17 22:24:50 +0700

    feat(auth): slice forgot password UI

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -7,30 +7,20 @@
                 <div class="d-flex flex-center flex-column flex-column-fluid pb-15 pb-lg-20">
                     <form class="form w-100" novalidate="novalidate" id="kt_sign_in_form" method="POST" action="#">
                         <div class="text-center mb-11">
-                            <h1 class="text-gray-900 fw-bolder mb-3">Sign In</h1>
-                            <div class="text-gray-500 fw-semibold fs-6">To Get Access to this application</div>
+                            <h1 class="text-gray-900 fw-bolder mb-3">Forgot Password</h1>
+                            <div class="text-gray-500 fw-semibold fs-6">Enter your phone number to reset your password</div>
                         </div>
                         <div class="fv-row mb-8">
                             <label for="phone_number">Phone Number</label>
                             <input type="text" placeholder="0812xxxx" name="phone_number" id="phone_number" class="form-control bg-transparent" required />
                         </div>
-                        <div class="fv-row mb-3">
-                            <label for="password">Password</label>
-                            <input type="password" placeholder="Password" name="password" autocomplete="off" class="form-control bg-transparent" required />
-                        </div>
-                        <div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
-                            <div></div>
-                            <a href="#" class="link-primary">Forgot Password ?</a>
-                        </div>
                         <div class="d-grid mb-10">
                             <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
-                                <span class="indicator-label">Sign In</span>
+                                <span class="indicator-label">Reset Password</span>
                                 <span class="indicator-progress">Please wait... 
                                 <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
                             </button>
                         </div>
-                        <div class="text-gray-500 text-center fw-semibold fs-6">I don't have an account. 
-                        <a href="#" class="link-primary">Sign up</a></div>
                     </form>
                 </div>
                 <div class="d-flex flex-stack">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -45,6 +45,7 @@
                         <a href="pages/team.html" target="_blank">Terms</a>
                         <a href="pages/contact.html" target="_blank">Contact Us</a>
                     </div>
+                </div>
             </div>
         </div>
     </div>
